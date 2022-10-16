8084aed53508f26fcce38c91db99ea2a632ea114
Memoona Mobeen <memoonamobeen612@gmail.com>
2022-10-16 16:24:24 +0500

    library

---
+++ src/index.js
@@ -0,0 +1,53 @@
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+//   res.json({"mona":222, })
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+const express = require("express");
+const app = express();
+const path = require("path");
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.urlencoded({extended:true}));
+const port = 5000
+//  console.log(path.join(__dirname, '../public'))
+const staticPath = path.join(__dirname, "../public");
+app.use(express.static(staticPath));
+
+
+
+app.get("/", (req, res)=>{
+    res.send("<h1>Hi there, Today m focusing on a JavaScript runtime built on Chrome's V8 JavaScript engine Likewise.</h1>");
+});
+// app.get("/about", (req, res)=>{
+//     res.send("<h1>Marvellous mona's keys :))</h1> ");
+// })
+// app.get("/contact", (req,res)=>{
+//     res.send("<h1>monamoon612@gmail.com</h1>");
+// })
+// app.get("/HomeTour", (req, res)=>{
+//     res.send("My Room Beauty :))")
+    
+// })
+// app.get("/BMI", (req, res)=>{
+//     res.sendFile(__dirname+"/index.html")
+// })
+// app.post("/BMI",(req,res)=>{
+//     console.log(req.body);
+//     let n1 = req.body.v1;
+//     let n2 = req.body.v2;
+// let  sum = n1 / (n1*n2);
+// res.send("<h1>Your BMI is: </h1>" + sum);
+// })
+ 
+app.listen(5000,  (req,res)=>{
+    console.log("server is running on 5000");
+    
+    
+});
---
+++ try.js
@@ -0,0 +1,16 @@
+// const express = require("express");
+// const app = express();
+// const port = 5000
+// app.get("/", (req, res)=>{
+//     res.send("its mona world!!!");
+// });
+// app.get("/about", (req, res)=>{
+//     res.send("Marvellous mona's keys :)) ");
+// });
+ 
+// app.listen(5000,  (req,res)=>{
+//     console.log("server is running on 5000");
+//     app.get("/contactus", (req, res)=>{
+//         res.send("Welcome,to mah world");
+//     });
+// });
